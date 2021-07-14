f19b153249500b582bf8de78623297ca354e9383
nicocarballo21 <nlucas120@gmail.com>
2021-07-14 15:50:01 -0300

    renderizado de login

--- front/MenteeMatch/src/App.js
+++ front/MenteeMatch/src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
-import {View, Text} from 'react-native';
-const App = () => {
-  return (
-    <View>
-      <Text>Inicio</Text>
-    </View>
-  );
-};
+import {Text, View} from 'react-native';
+import {NativeRouter, Route} from 'react-router-native';
+
+// components
+
+import Login from './screens';
+
+const App = () => (
+  <NativeRouter>
+    <Route exact path="/" component={Login} />
+  </NativeRouter>
+
+  // definit las rutas y los link como en react router web
+);
 
 export default App;
---
+++ front/MenteeMatch/src/screens/Login/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+const Login = () => {
+  return (
+    <View>
+      <Text style={{color: 'white'}}>Login</Text>
+    </View>
+  );
+};
+
+export default Login;
