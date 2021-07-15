f14c835968b85dc8ebb682f0243b428af4ac5715
guillesotelo <guille.sotelo.cloud@gmail.com>
2021-07-15 11:38:07 -0300

    nada

--- front/MenteeMatch/src/components/Home/index.js
+++ front/MenteeMatch/src/components/Home/index.js
@@ -1,15 +1,32 @@
 import React from 'react'
-import { View, Text } from 'react-native'
+import { View, Text, Button } from 'react-native'
 import styles from './styles'
 
 
 
 export default function Home() {
+
+    const shiftBestTwo = (arr) => {
+
+    }
+
+    const user = useSelector(store => store.user)
+    const users = useSelector(store => store.users)
+    let posibles = shiftBestTwo(users, user)
+
+    const handlePressBlock = () => {
+        console.log("Presionaste en UserBlock")
+        //Pushear
+    }
+
     return (
         <View style={styles.container}>
             <Text style={styles.title}>Hola, [userName]. {"\n"}Estos son tus posibles matches:</Text>
-            {/* MAPEO, SI EXISTEN, BLOCKES DE USUARIOS DE POSIBLES MATCH */}
-            
+            {/* MAPEO, SI EXISTEN, BLOQUES DE USUARIOS DE POSIBLES MATCH */}
+            <Button onPress={handlePressBlock}>
+
+            </Button>
+
             <Text style={{ textAlign: "center"}}>{"\n"}User 1</Text>
             <Text style={{ textAlign: "center"}}>{"\n"}User 2</Text>
         </View>
