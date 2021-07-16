ee2d1e570e62d5772c73729ce3a3861c4bb97f0c
Rafael <rafavillalopez@gmail.com>
2021-07-16 10:19:54 -0300

    npm run seed

--- front/MenteeMatch/src/components/index.js
+++ front/MenteeMatch/src/components/index.js
@@ -1,4 +1,4 @@
-import UserBlock from './UserBlock'
-import Matcher from './Matcher'
+import UserBlock from './UserBlock';
+import Matcher from './Matcher';
 // console.log("Matcher adentro de Index", Matcher)
-export { Matcher, UserBlock } 
+export {Matcher, UserBlock};
