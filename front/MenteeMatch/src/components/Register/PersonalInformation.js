c4b992910a2e9c8f6ae21232d3af6317300f27b2
nicocarballo21 <nlucas120@gmail.com>
2021-07-19 18:44:31 -0300

    arrglo de campos de userdata

--- front/MenteeMatch/src/components/Register/PersonalInformation.js
+++ front/MenteeMatch/src/components/Register/PersonalInformation.js
@@ -1,7 +1,9 @@
 import React from 'react';
-import { Text, View, TextInput, Button } from 'react-native';
+import { Text, View } from 'react-native';
 import { useForm, Controller } from 'react-hook-form';
 import styles from './styles';
+
+import { InputText, Button } from '../../components';
 
 export default ({ onSubmit }) => {
   const {
@@ -15,75 +17,81 @@ export default ({ onSubmit }) => {
       <Text style={styles.buttonText}>Datos Personales</Text>
 
       <View style={styles.inputs}>
+        {errors.country && (
+          <Text style={styles.error}>{errors.country.message}</Text>
+        )}
         <Controller
           control={control}
-          rules={{ required: true }}
+          rules={{ required: 'Campo Requerido' }}
           render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={styles.input}
+            <InputText
+              errors={errors.country}
               onBlur={onBlur}
               onChangeText={onChange}
               value={value}
-              placeholder="País"
+              placeholder="Pais"
             />
           )}
           name="country"
           defaultValue=""
         />
-        {errors.country && <Text>Requiere País.</Text>}
-
+        {errors.position && (
+          <Text style={styles.error}>{errors.position.message}</Text>
+        )}
         <Controller
           control={control}
-          rules={{ required: true }}
+          rules={{ required: 'Campo Requerido' }}
           render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={styles.input}
+            <InputText
+              errors={errors.position}
               onBlur={onBlur}
               onChangeText={onChange}
               value={value}
-              placeholder="Puesto"
+              placeholder="position"
             />
           )}
           name="position"
           defaultValue=""
         />
-        {errors.position && <Text>Requiere Puesto.</Text>}
-
+        {errors.phone && (
+          <Text style={styles.error}>{errors.phone.message}</Text>
+        )}
         <Controller
           control={control}
-          rules={{ required: true }}
+          rules={{ required: 'Campo Requerido' }}
           render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={styles.input}
+            <InputText
+              errors={errors.phone}
               onBlur={onBlur}
               onChangeText={onChange}
               value={value}
+              keyboardType="phone-pad"
               placeholder="011-1111-1111"
             />
           )}
           name="phone"
           defaultValue=""
         />
-        {errors.phone && <Text>Requiere Teléfono.</Text>}
-
+        {errors.about && (
+          <Text style={styles.error}>{errors.about.message}</Text>
+        )}
         <Controller
           control={control}
-          rules={{ required: true }}
+          rules={{ required: 'Campo Requerido' }}
           render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              style={styles.input}
+            <InputText
+              errors={errors.about}
               onBlur={onBlur}
               onChangeText={onChange}
               value={value}
-              placeholder="Acerca de..."
+              placeholder="Acerca de ..."
             />
           )}
           name="about"
           defaultValue=""
         />
-        {errors.about && <Text>Requiere Acerce de ...</Text>}
-
-        <Button title="Siguiete" onPress={handleSubmit(onSubmit)} />
+        \
+        <Button title="Siguiete" pressFunction={handleSubmit(onSubmit)} />
       </View>
     </View>
   );
--- front/MenteeMatch/src/screens/UserData/index.js
+++ front/MenteeMatch/src/screens/UserData/index.js
@@ -3,9 +3,19 @@ import { View } from 'react-native';
 import { sendUserData } from '../../services/reduxServices';
 import PersonalInformation from '../../components/Register/PersonalInformation';
 
+import { simpleMessage } from '../../utils';
+
 export default () => {
   const onSubmit = data => {
-    sendUserData(data);
+    sendUserData(data)
+      .then(() =>
+        simpleMessage(
+          'Registro exitoso',
+          'Su usuario fue registrado correctamente',
+          'success',
+        ),
+      )
+      .catch(() => simpleMessage('Error', 'Algo ha ocurrido', 'danger'));
   };
 
   return (
