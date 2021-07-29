2da6565c4fb525b36ff3eec114f76145a87f4211
Rafael <rafavillalopez@gmail.com>
2021-07-29 11:06:20 -0300

    arreglo vista checkbox

--- front/MenteeMatch/src/components/CheckBoxText/index.js
+++ front/MenteeMatch/src/components/CheckBoxText/index.js
@@ -13,10 +13,13 @@ function ChecKBoxText({ text, isChecked, onPress, mode }) {
         iconStyle={{ borderColor: mode.green }}
         textContainerStyle={{
           ...styles.option,
-          borderColor: mode.grey,
-          backgroundColor: mode.lightgray,
+          borderColor: mode.green,
+          backgroundColor: mode.bg,
         }}
-        textStyle={{ ...styles.text, color: mode.text }}
+        textStyle={{
+          ...styles.text,
+          color: mode.text,
+        }}
         disableBuiltInState
         isChecked={isChecked}
         onPress={onPress}
