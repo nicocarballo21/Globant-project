61285e58534a6064baba60d14a7be93759327d11
InevitableDesire <emanuelamicoo@gmail.com>
2021-08-02 14:22:12 -0300

    little fix and refactor

--- back/db/models/User.js
+++ back/db/models/User.js
@@ -129,7 +129,7 @@ userSchema.methods.comparePassword = function (password) {
 };
 
 userSchema.virtual("disponible").get(function () {
-  return this.mentees.length <= this.maxMentees;
+  return this.mentees.length < this.maxMentees;
 });
 
 const Users = mongoose.model("Users", userSchema);
