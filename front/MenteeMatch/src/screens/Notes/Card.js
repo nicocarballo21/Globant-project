04c94c4712f4e9b1e59f87a1fa0af2f41b4f3aed
nicocarballo21 <nlucas120@gmail.com>
2021-08-03 17:59:28 -0300

    handlersfunctions add to card component

--- front/MenteeMatch/src/screens/Notes/Card.js
+++ front/MenteeMatch/src/screens/Notes/Card.js
@@ -1,18 +1,64 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { View, Text, StyleSheet, ScrollView, Image } from 'react-native';
 import { Card, Button, Icon } from 'react-native-elements';
 
 import useMode from '../../hooks/useMode';
 
-export default function CardComponent({ mentee, notes }) {
+const data = [
+  {
+    title: 'titulo',
+    description: 'este es el cuerpo de la notas. ',
+  },
+  {
+    title: 'titulo',
+    description: 'este es el cuerpo de la notas. ',
+  },
+  {
+    title: 'titulo',
+    description: 'este es el cuerpo de la notas. ',
+  },
+  {
+    title: 'titulo',
+    description: 'este es el cuerpo de la notas. ',
+  },
+  {
+    title: 'titulo',
+    description: 'este es el cuerpo de la notas. ',
+  },
+  {
+    title: 'titulo',
+    description: 'este es el cuerpo de la notas. ',
+  },
+  {
+    title: 'titulo',
+    description: 'este es el cuerpo de la notas. ',
+  },
+  {
+    title: 'titulo',
+    description: 'este es el cuerpo de la notas. ',
+  },
+];
+export default function CardComponent({ mentee, functions }) {
+  const { handleAdd, handleEdit, handleDelete } = functions;
+  const [notes, setnotes] = useState([]);
   const { mode } = useMode();
+
+  //   useEffect(() => {
+  //     const get = async () => {
+  //       //hacerlo aca para subcrisbirse a los cambios que haya en el get de las notas y actualizar la screen
+  //       // hacer el get de las notas al back
+  //       // setnotes(notas);
+  //     };
+
+  //     get();
+  //   }, []);
 
   return (
     <ScrollView>
       <Card>
         <Card.Title>{`${mentee.name} ${mentee.surname}`}</Card.Title>
         <Card.Divider />
-        {notes.map((u, i) => {
+        {data.map((u, i) => {
           return (
             <View key={i} style={styles.card}>
               <Text style={styles.title}>{u.title}</Text>
@@ -26,10 +72,17 @@ export default function CardComponent({ mentee, notes }) {
                 <Button
                   icon={<Icon name="edit" color="#ffffff" />}
                   buttonStyle={{ ...styles.btns, backgroundColor: mode.green }}
+                  onPress={() => handleEdit()}
                 />
                 <Button
                   icon={<Icon name="delete" color="#ffffff" />}
                   buttonStyle={{ ...styles.btns, backgroundColor: mode.green }}
+                  onPress={() => handleDelete()}
+                />
+                <Button
+                  icon={<Icon name="add" color="#ffffff" />}
+                  buttonStyle={{ ...styles.btns, backgroundColor: mode.green }}
+                  onPress={() => handleAdd()}
                 />
               </View>
             </View>
--- front/MenteeMatch/src/screens/Notes/index.js
+++ front/MenteeMatch/src/screens/Notes/index.js
@@ -2,88 +2,49 @@ import React, { useState, useEffect } from 'react';
 import { View } from 'react-native';
 import Card from './Card';
 
-import { simpleMessage } from '../../utils/index';
-
-const data = [
-  {
-    title: 'titulo',
-    description: 'este es el cuerpo de la notas. ',
-  },
-  {
-    title: 'titulo',
-    description: 'este es el cuerpo de la notas. ',
-  },
-  {
-    title: 'titulo',
-    description: 'este es el cuerpo de la notas. ',
-  },
-  {
-    title: 'titulo',
-    description: 'este es el cuerpo de la notas. ',
-  },
-  {
-    title: 'titulo',
-    description: 'este es el cuerpo de la notas. ',
-  },
-  {
-    title: 'titulo',
-    description: 'este es el cuerpo de la notas. ',
-  },
-  {
-    title: 'titulo',
-    description: 'este es el cuerpo de la notas. ',
-  },
-  {
-    title: 'titulo',
-    description: 'este es el cuerpo de la notas. ',
-  },
-];
+// import { simpleMessage } from '../../utils/index';
 
 const Notes = ({ route }) => {
   const mentee = route.params.mente;
-  const [notes, setnotes] = useState([]);
 
-  useEffect(() => {
-    const get = async () => {
-      // hacer el get de las notas al back`
-      // setnotes(notas);
-    };
+  const handleAdd = () => {
+    // const res = await postObjectivesToUser(mentee._id, userToken, objective);
 
-    get();
-  }, []);
+    // if (res) {
+    //   simpleMessage(
+    //     'Objetivo agregado',
+    //     'El objetivo fue agregado correctamente',
+    //     'success',
+    //   );
+    //   setstate(!state);
+    // }
+    console.log('add');
+  };
 
-  // const handleAdd = async objective => {
-  //   const res = await postObjectivesToUser(mentee._id, userToken, objective);
+  const handleDelete = () => {
+    // const res = await deleteObjectivesToUser(mentee._id, userToken, objetiveId);
+    // if (res) setstate(!state);
+    console.log('delete');
+  };
 
-  //   if (res) {
-  //     simpleMessage(
-  //       'Objetivo agregado',
-  //       'El objetivo fue agregado correctamente',
-  //       'success',
-  //     );
-  //     setstate(!state);
-  //   }
-  // };
-
-  // const handleDelete = async objetiveId => {
-  //   const res = await deleteObjectivesToUser(mentee._id, userToken, objetiveId);
-  //   if (res) setstate(!state);
-  // };
-
-  // const handleEdit = async (objectiveId, data) => {
-  //   const res = updateObjectivesToUser(userToken, objectiveId, data);
-  //   if (res) {
-  //     simpleMessage(
-  //       'Objetivo editado',
-  //       'El objetivo fue editado correctamente',
-  //       'success',
-  //     );
-  //     setstate(!state);
-  //   }
-  // };
+  const handleEdit = () => {
+    // const res = updateObjectivesToUser(userToken, objectiveId, data);
+    // if (res) {
+    //   simpleMessage(
+    //     'Objetivo editado',
+    //     'El objetivo fue editado correctamente',
+    //     'success',
+    //   );
+    //   setstate(!state);
+    // }
+    console.log('edit1');
+  };
   return (
     <View>
-      <Card notes={data} mentee={mentee} />
+      <Card
+        mentee={mentee}
+        functions={{ handleAdd, handleDelete, handleEdit }}
+      />
     </View>
   );
 };
