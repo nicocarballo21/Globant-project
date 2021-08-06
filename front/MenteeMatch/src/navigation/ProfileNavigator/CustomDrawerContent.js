1ae9b444e1f00cae08aaf15cd35f4a3a58c059d8
Rafael <rafavillalopez@gmail.com>
2021-08-06 10:19:25 -0300

    mode oscuro

--- front/MenteeMatch/src/navigation/ProfileNavigator/CustomDrawerContent.js
+++ front/MenteeMatch/src/navigation/ProfileNavigator/CustomDrawerContent.js
@@ -6,7 +6,6 @@ import {
 } from '@react-navigation/drawer';
 
 import { useDispatch } from 'react-redux';
-import Switch from '../../components/Switch';
 import { removeData } from '../../utils/storage';
 import { setUser } from '../../redux/Reducers/UserReducer';
 import { logout } from '../../redux/Slices/authSlice';
@@ -14,7 +13,6 @@ import { setSkills } from '../../redux/Reducers/Skills';
 import { setMatches } from '../../redux/Reducers/matchesReducer';
 import useMode from '../../hooks/useMode';
 import Ionicons from 'react-native-vector-icons/Ionicons';
-
 
 export default function CustomDrawerContent(props) {
   const { mode } = useMode();
@@ -39,15 +37,14 @@ export default function CustomDrawerContent(props) {
         label="Cerrar sesion"
         labelStyle={{ color: mode.text }}
         onPress={handleLogOut}
-        icon={() => <Ionicons name="log-out" size={24} /> }
+        icon={() => <Ionicons name="log-out" size={24} />}
       />
       <DrawerItem
         label="Mis reuniones"
         labelStyle={{ color: mode.text }}
         onPress={() => props.navigation.navigate('Meets')}
-        icon={() => <Ionicons name="calendar" size={24} /> }
+        icon={() => <Ionicons name="calendar" size={24} />}
       />
-      <Switch />
     </DrawerContentScrollView>
   );
 }
