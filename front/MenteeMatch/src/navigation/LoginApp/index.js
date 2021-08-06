57916073bf9647ee1a18111d84632bbf703f4923
InevitableDesire <emanuelamicoo@gmail.com>
2021-08-06 11:15:13 -0300

    fix header register arrow

--- front/MenteeMatch/src/navigation/LoginApp/index.js
+++ front/MenteeMatch/src/navigation/LoginApp/index.js
@@ -13,13 +13,46 @@ const Stack = createStackNavigator();
 
 export default function LoginApp() {
   return (
-    <Stack.Navigator initialRouteName="FirstScreen" headerMode={'none'}>
-      <Stack.Screen name="FirstScreen" component={FirstScreen} />
-      <Stack.Screen name="Login" component={Login} />
-      <Stack.Screen name="Register" component={Register} />
-      <Stack.Screen name="UserData" component={UserData} />
-      <Stack.Screen name="RoleSelection" component={RoleSelection} />
-      <Stack.Screen name="SelectSkills" component={SelectSkills} />
+    <Stack.Navigator
+      initialRouteName="FirstScreen"
+      screenOptions={{
+        headerStyle: { backgroundColor: '#BFD732' },
+        headerTintColor: '#F5F6F7',
+        headerTitleAlign: 'center',
+        headerTitleStyle: {
+          fontWeight: 'bold',
+        },
+      }}>
+      <Stack.Screen
+        name="FirstScreen"
+        component={FirstScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Login"
+        component={Login}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Register"
+        component={Register}
+        options={{ title: 'Registro' }}
+      />
+      <Stack.Screen
+        name="UserData"
+        component={UserData}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="RoleSelection"
+        component={RoleSelection}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="SelectSkills"
+        component={SelectSkills}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 }
